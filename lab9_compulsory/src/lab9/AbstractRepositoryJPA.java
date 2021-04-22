6ee1bfb17bad680b0f89e29ddacf34c2e7551401
Gheorghițanu Bianca-Gabriela <79106145+biancaggh@users.noreply.github.com>
2021-04-22 09:38:32 +0300

    lab9

---
+++ lab9_compulsory/src/lab9/AbstractRepositoryJPA.java
@@ -0,0 +1,18 @@
+package lab9;
+
+import java.util.List;
+
+class AbstractRepositoryClassJPA implements AbstractRepository {
+
+    public void createEntity(Object o) {
+
+    }
+
+    public Object findById(String id) {
+        return null;
+    }
+
+    public List findByName(String name) {
+        return null;
+    }
+}
---
+++ lab9_compulsory/src/lab9/Movie.java
@@ -0,0 +1,25 @@
+package lab9;
+
+import java.sql.Date;
+
+public class Movie {
+    private int id;
+    private String title;
+    private Date release_date;
+    private String duration;
+    private double score;
+
+    public int getId(){return this.id;}
+    public String getTitle() {return this.title;}
+    public Date getRelease_date() { return this.release_date;}
+    public String getDuration(){ return this.duration;}
+    public double getScore() {return this.score;}
+
+    public Movie(int id, String title, Date release_date, String duration, double score){
+        this.id=id;
+        this.title=title;
+        this.release_date=release_date;
+        this.duration=duration;
+        this.score=score;
+    }
+}
